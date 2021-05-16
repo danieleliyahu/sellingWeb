e35066a4cd43c7fa3b955ae45eafd98b4d0fe4ab
danieleliyho <77384369+danieleliyho@users.noreply.github.com>
2021-05-16 19:29:36 +0300

    place order and shipping screen

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -7,6 +7,9 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import SigninScreen from "./screens/SigninScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 function App() {
   const cart = useSelector(state=> state.cart);
@@ -50,6 +53,9 @@ function App() {
         <Route path="/cart/:id?" component={CartScreen}></Route>
         <Route path="/signin" component={SigninScreen}></Route>
         <Route path="/register" component={RegisterScreen}></Route>
+        <Route path="/shipping" component={ShippingAddressScreen}></Route>
+        <Route path="/payment" component={PaymentMethodScreen}></Route>
+        <Route path="/placeorder" component={PlaceOrderScreen}></Route>
         <Route path="/" component={HomeScreen} exact></Route>
        
     </main>
--- amazona/frontend/src/store.js
+++ amazona/frontend/src/store.js
@@ -12,7 +12,11 @@ const initialState = {
 cart:{
     cartItems: localStorage.getItem('cartItems')?
     JSON.parse(localStorage.getItem('cartItems')):
-    []
+    [],
+    shippingAddress: localStorage.getItem('shippingAddress')
+    ? JSON.parse(localStorage.getItem('shippingAddress'))
+    :{},
+    paymentMethod: 'paypal',
 }
 };
 const reducer = combineReducers({

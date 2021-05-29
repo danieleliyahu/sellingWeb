3ad6a2943a4e1a39ce47cb31d76107baf693ede7
danieleliyahu <77384369+danieleliyahu@users.noreply.github.com>
2021-05-29 19:46:02 +0300

    add product

--- amazona/frontend/src/store.js
+++ amazona/frontend/src/store.js
@@ -5,6 +5,7 @@ import {
   productCreateReducer,
   productDetailsReducer,
   productListReducer,
+  productUpdateReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -48,6 +49,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
